ca1322bbdf8fd7366c03ae052ffe1e35b1b864ab
Layse Santos Souza <99034673+SouzaLayse@users.noreply.github.com>
2022-02-13 13:41:04 -0300

    Add files via upload

---
+++ Aula0.java
@@ -0,0 +1,13 @@
+package Aula0.Aula0;
+
+public class Aula0 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Boa tarde! Nova linha.");
+        System.out.print("Este texto ");
+        System.out.print("na mesma linha!");  
+
+    }
+    
+}
