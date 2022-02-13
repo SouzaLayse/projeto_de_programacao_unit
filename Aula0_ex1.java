b819261e6f35b5a81cbc19cf62c9961322853e97
Layse Santos Souza <99034673+SouzaLayse@users.noreply.github.com>
2022-02-13 14:17:20 -0300

    Add files via upload

---
+++ Aula0_ex1.java
@@ -0,0 +1,21 @@
+package Aula0.Aula0;
+
+public class Aula0_ex1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int var1; //declaração de variável
+        int var2; //declaração de variável
+
+        var1 = 1024; //atribuição de um valor a variável
+
+        System.out.println("Conteúdo de var1: " + var1);
+
+        var2 = var1 / 2;
+
+        System.out.println("Conteúdo de var2: " + var2);
+
+    }
+    
+}
