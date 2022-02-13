738ab116f5652344230b2f15c93adde4be89893d
Layse Santos Souza <99034673+SouzaLayse@users.noreply.github.com>
2022-02-13 14:22:11 -0300

    Add files via upload

---
+++ Aula0_exSelecao.java
@@ -0,0 +1,20 @@
+package Aula0.Aula0;
+
+public class Aula0_exSelecao {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int a, b;
+        a = 2;
+        b = 3;
+        
+        if (a < b) {
+            System.out.println("a é menor que b");
+        } else {
+            System.out.println("b é menor que a");
+        }
+        
+    }
+    
+}
