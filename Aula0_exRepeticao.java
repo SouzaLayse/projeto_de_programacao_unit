e819103d979b0a52dd475ad0d03477b3b981c8b5
Layse Santos Souza <99034673+SouzaLayse@users.noreply.github.com>
2022-02-13 14:24:22 -0300

    Add files via upload

---
+++ Aula0_exRepeticao.java
@@ -0,0 +1,13 @@
+package Aula0.Aula0;
+
+public class Aula0_exRepeticao {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        for (int count = 0; count < 5; count++) {
+            System.out.println("Contador: " + count);
+        }
+    }
+    
+}
