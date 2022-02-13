5f8fa3509e71735d38303d5b6bf4bbb233a00fd3
Layse Santos Souza <99034673+SouzaLayse@users.noreply.github.com>
2022-02-13 12:10:40 -0300

    Add files via upload

---
+++ HelloWord.java
@@ -0,0 +1,10 @@
+package Aula0.Aula0;
+
+public class HelloWord {
+
+        public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+    
+}
